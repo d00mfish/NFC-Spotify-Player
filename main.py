bd340e3754ebcb0579cd0736524516a8d3324e10
d00mfish <dummfischlp@hotmail.de>
2021-02-06 16:39:38 +0100

    .

--- main.py
+++ main.py
@@ -6,44 +6,94 @@ import spotify_api as spotify
 print("Scan the learn-card to add a Playlist to the system.")
 print("Scan the setup-card to assign the current playing device as default.")
 
-while(True):
-    uid, str_uid = rfid.wait_for_uid()
-    #get_shuffle_state()
-    
-    
-    #Device Learning
-    if str_uid == spotify.device_card_uid:
-        device_id, device_name = spotify.current_device()
-        if device_id != -1:  # set device id if id sucessfull retrieved
-            spotify.set_config_value('AUTH','device_id',str(device_id))
-            print("Set {} as new device.".format(device_name))
+def main():
+    while(True):
+        uid, str_uid = rfid.check_once(0.5)#timeout controlls refresh time for loop
+        #get_shuffle_state()
+        if uid == -1:
+            continue
+
+        
+        
+        #Device Learning
+        if str_uid == spotify.device_card_uid:
+            device_id, device_name = spotify.current_device()
+            if device_id != -1:  # set device id if id sucessfull retrieved
+                spotify.set_config_value('AUTH','device_id',str(device_id))
+                print("Set {} as new device.".format(device_name))
+                time.sleep(2)
+                continue
+            print("No playback detected, can't set device.")
             time.sleep(2)
-            continue
-        print("No playback detected, can't set device.")
-        time.sleep(2)
+        
+        #Starting Card-Writing
+        elif str_uid == spotify.learn_card_uid:
+            if write_card() == -1:
+                print("Something went wrong writing the new Music-Card.")
+            time.sleep(2)
+            
+        #read data and play
+        else:
+            uri = rfid.read_uri(uid)
+            if uri == -1:
+                print("Problem while reading. Make sure you already added this Card.")
+                time.sleep(1)
+                continue
+            
+            print("Found Music-Card.")
+            if spotify.play_context_URI(uri) == -1:  # play uri playlist at device
+                print("Current device unavailable, please select an available device.") #maybe use fallback device afterwards?
+                print("This can happen if you use a Phone or PC that is not always online.")
+                time.sleep(1)
+                continue
+                
+            print("Playing now!")
+            time.sleep(1)
+
+
+
+
+def write_card():
+    # Get current playlist uri and playing song info
+    try:
+        current = spotify.current_playback()
+    except TypeError:
+        print('''
+        >Please play a track out of the playlist you want to learn.
+        >Can't be \"Liked Songs\" or Podcast-Shows.
+        >Aborting Learning.
+        ''')
+        return -1
+    print("")
+    print("Playing a playlist containing: {}, by {}.".format(
+        current[1], current[2]))
+    uri = current[0]
+
+    # preparing uri into byte arrays
+    parts = [uri[i:i+4] for i in range(0, len(uri), 4)]
+    print("Number of Parts: {}".format(len(parts)))
+    # making bytearrays with 16bytes of size, no matter what
+    data_arrays = [bytearray(i, 'utf-8')+bytearray(4-len(i)) for i in parts]
+    print(data_arrays)
+
+    time.sleep(3)
+
+    print("Scan and hold the card you want to learn now.")
+    print("Scan the learn-card again to abort.")
+    uid, str_uid = spotify.wait_for_uid()
+    if str_uid == spotify.learn_card_uid or str_uid == spotify.device_card_uid:
+        print("Can't write uri to learn or device card. Arborting!")
+        return -1
     
-    #Starting Card-Writing
-    elif str_uid == spotify.learn_card_uid:
-        if rfid.write_card(spotify.current_playback()) == -1:
-            print("Something went wrong writing the new Music-Card.")
-        time.sleep(2)
-        
-    #read data and play
-    else:
-        uri = rfid.read_uri(uid)
-        if uri == -1:
-            print("Problem while reading. Make sure you already added this Card.")
-            time.sleep(1)
-            continue
-        
-        print("Found Music-Card.")
-        if spotify.play_context_URI(uri) == -1:  # play uri playlist at device
-            print("Current device unavailable, please select an available device.") #maybe use fallback device afterwards?
-            print("This can happen if you use a Phone or PC that is not always online.")
-            time.sleep(1)
-            continue
+    for i in range(len(data_arrays)):
+        if rfid.write_block(i+10, data_arrays[i]) == -1:
+            print("Something went wrong writing.")
+            return -1
             
-        print("Playing now!")
-        time.sleep(1)
-        
-            
+    print("Successfully leaned!")
+
+
+
+
+if __name__ == '__main__':
+    main()
--- rfid_com.py
+++ rfid_com.py
@@ -31,104 +31,46 @@ def wait_for_uid():
     str_uid = ''.join(format(x, '0x') for x in uid)
     return uid, str_uid
 
+def check_once(timeout):
+    uid = pn532.read_passive_target(timeout=timeout)
+    if uid is not None:
+        str_uid = ''.join(format(x, '0x') for x in uid)
+        return uid, str_uid
+    return -1, -1
 
-def RFID_read(uid, block, read_key):
-    if read_key == 'A':
-        key = key_a
-        key_type = MIFARE_CMD_AUTH_A
-    elif read_key == 'B':
-        key = key_b
-        key_type = MIFARE_CMD_AUTH_B
-    else:
-        raise Exception("read_key must be 'A' or 'B'")
-
-    authenticated = pn532.mifare_classic_authenticate_block(uid, block, key_type, key)
-    if not authenticated:
-        print("Read Authentication failed!")
+def RFID_read(block):
+    data = pn532.ntag2xx_read_block(block)
+    if data is None:
         return -1
-    else:
-        return pn532.mifare_classic_read_block(block)
+    return data
 
 
 def read_uri(uid):
-    b1 = RFID_read(uid, 16, 'A')
-    b2 = RFID_read(uid, 17, 'A')
-    b3 = RFID_read(uid, 18, 'A')
-    b4 = RFID_read(uid, 20, 'A')
-    if -1 in (b1, b2, b3, b4):
-        print("Something went wrong reading.")
-        return -1
-    return (b1+b2+b3+b4).decode('utf-8').strip('\x00')
+    data_arrays = []
+    #while(data_arrays[-1] != bytearray(4)):
+    for i in range(10, 31):
+        array = RFID_read(i)
+        if array == bytearray(4):
+            return ''.join([x.decode('utf-8').strip('\x00') for x in data_arrays])
+        else:
+            data_arrays.append(array)
 
-
-def write_block(uid, block, data, write_key):  # Writekey must be A or B
-    if write_key == 'A':
-        key = key_a
-        key_type = MIFARE_CMD_AUTH_A
-    elif write_key == 'B':
-        key = key_b
-        key_type = MIFARE_CMD_AUTH_B
-    else:
-        raise Exception("write_key must be 'A' or 'B'")
-
-    authenticated = pn532.mifare_classic_authenticate_block(uid, block, key_type, key)
-    if not authenticated:
-        print("Write Authentication failed!")
-        return -1
-    else:
-        if pn532.mifare_classic_write_block(block, data) is False:
-            return -1
-
-
-def write_card(current):
-    # Get current playlist uri and playing song info
-    try:
-        print("")
-        print("Playing a playlist containing: {}, by {}.".format(
-            current[1], current[2]))
-        uri = current[0]
-    except TypeError:
-        print('''
-        >Please play a track out of the playlist you want to learn.
-        >Can't be \"Liked Songs\" or Podcast-Shows.
-        >Aborting Learning.
-        ''')
-        return -1
-
-    # preparing uri into byte arrays
-    parts = [uri[i:i+16] for i in range(0, len(uri), 16)]
-    # making bytearrays with 16bytes of size, no matter what
-    b1 = bytearray(parts[0], 'utf-8')+bytearray(16-len(parts[0]))
-    b2 = bytearray(parts[1], 'utf-8')+bytearray(16-len(parts[1]))
-    b3 = bytearray(parts[2], 'utf-8')+bytearray(16-len(parts[2]))
-    b4 = bytearray(parts[3], 'utf-8')+bytearray(16-len(parts[3]))
-
-    time.sleep(3)
-
-    print("Scan and hold the card you want to learn now.")
-    print("Scan the learn-card again to abort.")
-    uid, str_uid = wait_for_uid()
-    if str_uid == learn_card_uid or str_uid == device_card_uid:
-        print("Can't write uri to learn or device card. Arborting!")
-        return -1
+         
     
-    r1 = write_block(uid, 16, b1, 'A')
-    r2 = write_block(uid, 17, b2, 'A')
-    r3 = write_block(uid, 18, b3, 'A')
-    r4 = write_block(uid, 20, b4, 'A')
 
-    if -1 in (r1, r2, r3, r4):
-        print("Something went wrong writing.")
+
+def write_block(block, data):
+    if pn532.ntag2xx_write_block(block, data) is False:
         return -1
-    print("Successfully leaned!")
 
 
 if __name__ == "__main__":
 
     while(1):
         print("Scan and hold the card you want to learn now.")
-        uid = wait_for_uid()
+        uid, str_uid = wait_for_uid()
         print("UID-Found:{}".format(uid))
+        print(read_uri(uid))
         
         
         
