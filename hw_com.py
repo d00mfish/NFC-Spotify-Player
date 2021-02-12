ce0056a6ed7ca5b3494300ebedd64a5dfd93e28c
d00mfish <dummfischlp@hotmail.de>
2021-02-12 20:35:47 +0100

    .

--- hw_com.py
+++ hw_com.py
@@ -101,16 +101,20 @@ def blink_error():
 
 
 def blink_ok():
-    #def blink_ok_thread():
-    shuffle_before = GPIO.input(shuffle_led_pin)
-    playpause_before = GPIO.input(skip_led_pin)
-    for _ in range(5):
-        GPIO.output(skip_led_pin, True)
-        GPIO.output(shuffle_led_pin, True)
-        sleep(0.1)
-        GPIO.output(skip_led_pin, False)
-        GPIO.output(shuffle_led_pin, False)
-        sleep(0.1)
-    set_button_led(skip_led, playpause_before, 100)
-    set_button_led(shuffle_led, shuffle_before, 100)
-    #threading.Thread(target=blink_ok_thread).start()
+    def blink_ok_shuffle_thread():
+        shuffle_before = GPIO.input(shuffle_led_pin)
+        for _ in range(2):
+            GPIO.output(shuffle_led_pin, True)
+            sleep(0.1)
+            GPIO.output(shuffle_led_pin, False)
+        set_button_led(shuffle_led, shuffle_before, 100)
+    def blink_ok_skip_thread():
+        skip_before = GPIO.input(skip_led_pin)
+        for _ in range(2):
+            GPIO.output(skip_led_pin, True)
+            sleep(0.1)
+            GPIO.output(skip_led_pin, False)
+            sleep(0.1)
+        set_button_led(skip_led, skip_before, 100)
+    threading.Thread(target=blink_ok_shuffle_thread).start()
+    threading.Thread(target=blink_ok_skip_thread).start()
