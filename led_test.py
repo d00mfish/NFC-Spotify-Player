d0040264ff4fc24eb6c8462d7a5738921ee3114e
d00mfish <dummfischlp@hotmail.de>
2021-02-17 21:19:18 +0100

    .

--- led_test.py
+++ led_test.py
@@ -187,13 +187,16 @@ def set_led_dc(channel: object, dc):
 def blink_error():
     shuffle_before = get_led_state(shuffle_led)
     skip_before = get_led_state(skip_led)
-    set_button_led(skip_led, False, 300)
-    set_button_led(shuffle_led, False, 300)
+    set_button_led(skip_led, False, 70)
+    set_button_led(shuffle_led, False, 70)
+    sleep(0.1)
     for _ in range(3):
         set_button_led(skip_led, True, 300)
         set_button_led(shuffle_led, True, 300)
+        sleep(0.3)
         set_button_led(skip_led, False, 300)
         set_button_led(shuffle_led, False, 300)
+        sleep(0.3)
     set_button_led(shuffle_led, shuffle_before, 300)
     set_button_led(skip_led, skip_before, 300)
 
