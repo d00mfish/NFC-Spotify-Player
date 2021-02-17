7cfb9b14e56cebb54cba91c952eff8a3971101c5
d00mfish <dummfischlp@hotmail.de>
2021-02-17 21:18:17 +0100

    .

--- led_test.py
+++ led_test.py
@@ -54,7 +54,7 @@ def convert_value(inputval, maxinput, maxoutput):
 
 
 def get_led_state(channel):
-    return pi.read(channel)
+    return pi.read(channel)*100
 
 
 def set_button_led(channel: int, state: bool, speed_ms: int):
@@ -185,41 +185,34 @@ def set_led_dc(channel: object, dc):
 
 
 def blink_error():
-    def blink_err_thread():
-        shuffle_before = get_led_state(shuffle_led)
-        skip_before = get_led_state(skip_led)
+    shuffle_before = get_led_state(shuffle_led)
+    skip_before = get_led_state(skip_led)
+    set_button_led(skip_led, False, 300)
+    set_button_led(shuffle_led, False, 300)
+    for _ in range(3):
+        set_button_led(skip_led, True, 300)
+        set_button_led(shuffle_led, True, 300)
         set_button_led(skip_led, False, 300)
         set_button_led(shuffle_led, False, 300)
-        for _ in range(3):
-            set_button_led(skip_led, True, 300)
-            set_button_led(shuffle_led, True, 300)
-            set_button_led(skip_led, False, 300)
-            set_button_led(shuffle_led, False, 300)
-        set_button_led(shuffle_led, shuffle_before, 300)
-        set_button_led(skip_led, skip_before, 300)
-
-    threading.Thread(target=blink_err_thread).start()
+    set_button_led(shuffle_led, shuffle_before, 300)
+    set_button_led(skip_led, skip_before, 300)
 
 
 def blink_ok():
-    def blink_ok_thread():
-        shuffle_before = get_led_state(shuffle_led)
-        skip_before = get_led_state(skip_led)
-        set_button_led(skip_led, False, 0)
-        set_button_led(shuffle_led, False, 0)
-        sleep(0.1)
-        set_button_led(skip_led, True, 100)
-        set_button_led(skip_led, False, 0)
-        set_button_led(shuffle_led, True, 100)
-        set_button_led(shuffle_led, False, 0)
-        set_button_led(skip_led, True, 100)
-        set_button_led(skip_led, False, 0)
-        if skip_before:
-            set_button_led(skip_led, skip_before, 100)
-        if shuffle_before:
-            set_button_led(shuffle_led, shuffle_before, 100)
-
-    threading.Thread(target=blink_ok_thread).start()
+    shuffle_before = get_led_state(shuffle_led)
+    skip_before = get_led_state(skip_led)
+    set_button_led(skip_led, False, 70)
+    set_button_led(shuffle_led, False, 70)
+    sleep(0.1)
+    set_button_led(skip_led, True, 70)
+    set_button_led(skip_led, False, 0)
+    set_button_led(shuffle_led, True, 70)
+    set_button_led(shuffle_led, False, 0)
+    set_button_led(skip_led, True, 70)
+    set_button_led(skip_led, False, 0)
+    sleep(0.1)
+    set_button_led(skip_led, skip_before, 100)
+    set_button_led(shuffle_led, shuffle_before, 100)
 
 
 if __name__ == "__main__":
